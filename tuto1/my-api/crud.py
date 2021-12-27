a3d00fa1545df0e2485e9dae5eecb117a21ba3fc
leoxz98 <32520391+leoxz98@users.noreply.github.com>
2021-12-27 00:41:33 -0300

    Delete tuto1/my-api directory

--- tuto1/my-api/crud.py
+++
@@ -1,9 +0,0 @@
-from sqlalchemy.orm import Session
-
-from . import models, schemas
-
-
-def get_new(db: Session, new_id: int):
-    return db.query(models.New).filter(models.New.id == new_id).first()
-
-
